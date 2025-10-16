b4f2463bece833d8a39e809c527f11d23c210b37
Brittany Price <bprice0496@gmail.com>
2025-10-16 19:48:47 -0400

    App & main setup

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,13 +1,25 @@
 import './css/App.css';
 import { Outlet, useLocation } from 'react-router-dom';
 
+import AppStyles from "styled-components";
 
+const Apps = AppStyles.div`
+    height: 900px;
+    overflow: hidden;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    gap: 40px;
+`;
 
 function App() {
 
   return (
     <>
- 
+      <Apps>
+        <Outlet></Outlet>
+      </Apps>
     </>
   )
 }
