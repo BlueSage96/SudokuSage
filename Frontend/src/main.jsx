5dfb2c74716e6ccabdc629428bd583868a2b08fc
Brittany Price <bprice0496@gmail.com>
2026-02-02 23:04:38 -0500

    Routing setup

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -1,10 +1,21 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from 'react-router-dom';
+import './index.css';
+import App from './App.jsx';
+import Menu from "./Components/Pages/Menu";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+const routes = createBrowserRouter(
+  createRoutesFromElements(
+     <Route path="" element={<App/>}>
+      <Route path="/" element={<Menu/>}/>
+     </Route>
+  )
 )
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <RouterProvider router={routes}>
+      <App />
+    </RouterProvider>
+  </StrictMode>
+);
