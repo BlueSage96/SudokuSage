e22a64601b593509d3cdef9fa3c6a35988d9bde9
Brittany Price <bprice0496@gmail.com>
2026-02-02 23:30:19 -0500

    Go background setup

--- Frontend/src/Components/Pages/Menu.jsx
+++ Frontend/src/Components/Pages/Menu.jsx
@@ -1,6 +1,10 @@
+import MenuStyles from "../../css/Menu.module.css";
+import BG from "../../assets/background.png";
+
 export default function Menu () {
     return (
         <>
+           <img src={BG} className={MenuStyles.Background} alt="Main menu background image"/>
            
         </>
     )
