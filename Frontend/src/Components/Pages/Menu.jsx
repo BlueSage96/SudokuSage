2fa49e72c0e3b225886d5ac2a737982a9d5ff5ea
Brittany Price <bprice0496@gmail.com>
2026-02-03 00:00:32 -0500

    Added login button

--- Frontend/src/Components/Pages/Menu.jsx
+++ Frontend/src/Components/Pages/Menu.jsx
@@ -1,11 +1,15 @@
+import { Button } from "flowbite-react";
 import MenuStyles from "../../css/Menu.module.css";
 import BG from "../../assets/background.png";
+import SignIn from "../../assets/SignIn1.png";
 
 export default function Menu () {
     return (
         <>
            <img src={BG} className={MenuStyles.Background} alt="Main menu background image"/>
-           
+            <Button>
+                <img src={SignIn} className={MenuStyles.SignIn}/>
+            </Button>
         </>
     )
 }
