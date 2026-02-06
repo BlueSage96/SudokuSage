972dcdf0ae228414e73b5b6c69b258d068c59879
Brittany Price <bprice0496@gmail.com>
2026-02-06 12:30:30 -0500

    Init auth routers

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -4,14 +4,19 @@ import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } 
 import './index.css';
 import App from './App.jsx';
 import Menu from "./Components/Pages/Menu";
+import Auth from "./Auth/Auth";
 
 const routes = createBrowserRouter(
   createRoutesFromElements(
-     <Route path="" element={<App/>}>
-      <Route path="/" element={<Menu/>}/>
-     </Route>
-  )
-)
+    <Route path="" element={<App />}>
+      <Route path="/" element={<Menu />} />
+      <Route path="/auth" element={<Auth />} />
+      {/* <Route path="forgotPwd" element={<ForgotPassword />} />
+      <Route path="/game/:id" element={<Game />} />
+      <Route path="/setup" element={<GameFunc />} /> */}
+    </Route>,
+  ),
+);
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <RouterProvider router={routes}>
