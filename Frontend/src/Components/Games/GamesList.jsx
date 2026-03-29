19f98822c72ec1f14b7f82fd6b34003e30e59390
Brittany Price <bprice0496@gmail.com>
2026-03-29 01:49:15 -0400

    Working check and checkbox

--- Frontend/src/Components/Games/GamesList.jsx
+++ Frontend/src/Components/Games/GamesList.jsx
@@ -1,19 +1,39 @@
-import { Card, Button } from "flowbite-react";
+import { Card, Button, Checkbox } from "flowbite-react";
+import { Check } from "lucide-react";
 export default function GamesList () {
     return (
       <>
         <div>
-          <Card className="card-container items-center justify-center text-2xl font-bold border-2">
+          <Card className="card-container items-center justify-center font-bold border-2">
             <div className="flex gap-12">
-              <Button className="select-style"></Button>
-              <h1>Games</h1>
-              <Button className="btn-style-3">+New</Button>
+              <label className="flex items-center gap-2 cursor-pointer">
+                <div className="relative">
+                  <Checkbox className="peer appearance-none select-style" />
+
+                  {/* Custom checkmark */}
+                  <Check className="check-style" />
+                </div>
+              </label>
+              <h1 className="text-2xl">Games</h1>
+              <Button className="btn-style-3 !relative right-[20px]">
+                +New
+              </Button>
             </div>
 
+            {/* Make into it's own reusable modal */}
             <div className="flex gap-12">
-              <Button className="select-style"></Button>
-              <h1>Game 1</h1>
-              <Button className="btn-style-3">Edit</Button>
+              <label className="flex items-center gap-2 cursor-pointer">
+                <div className="relative">
+                  <Checkbox className="peer appearance-none select-style" />
+
+                  {/* Custom checkmark */}
+                  <Check className="check-style" />
+                </div>
+              </label>
+              <h1 className="text-xl">Game 1</h1>
+              <Button className="btn-style-3 !relative bottom-[10px] right-[5px]">
+                Edit
+              </Button>
             </div>
           </Card>
         </div>
